0875cf3f55e88c754a23240a96846c21834a4f47
Leif <yzluofei@126.com>
2018-11-11 12:56:58 +0800

    新增日志记录功能，并且实现在Spring Boot Admin中查看

--- eureka-client/src/main/java/com/mecaptain/eurekaclient/HiController.java
+++ eureka-client/src/main/java/com/mecaptain/eurekaclient/HiController.java
@@ -1,5 +1,6 @@
 package com.mecaptain.eurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,12 @@ public class HiController {
     String port;
 
     @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam String name) {
         return "hi " + name + ",i am from port:" + port;
     }
 
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
 }
